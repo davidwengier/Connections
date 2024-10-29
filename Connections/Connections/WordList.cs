fb45ce9a5db1ec99634fa7cf541a5cca66f00f6f
David Wengier <david.wengier@microsoft.com>
2024-10-29 22:00:46 +1100

    Add project files.

---
+++ Connections/Connections/WordList.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Connections;
+
+public record Game
+{
+    [JsonPropertyName("startingGroups")]
+    public required string[][] StartingGroups { get; init; }
+
+    [JsonPropertyName("groups")]
+    public required Dictionary<string, WordGroup> Groups { get; init; }
+}
+
+public record WordGroup(
+    [property: JsonPropertyName("level")]
+    int Level,
+
+    [property: JsonPropertyName("members")]
+    string[] Members);
